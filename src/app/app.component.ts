ae9fcd1018216234f14296e4bf624e0fc5293c5e
cedrickdeguzman <deguzmancedrick95@gmail.com>
2019-12-13 19:27:16 +0700

    refactor(appComponent): get data from service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
+import { ToDo } from './model/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo';
+  toDos: ToDo[] = [];
+
+  constructor(
+    private toDoService: TodoService
+  ) {}
+
+  ngOnInit() {
+    this.title = 'anchor';
+    this.getData();
+  }
+
+  getData() {
+    console.log(this.toDos,'awuuu');
+    this.toDos = this.toDoService.toDoData();
+    console.log(this.toDos);
+  }
 }
