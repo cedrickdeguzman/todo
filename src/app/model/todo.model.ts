34caf5a9774e6692c0c464b93ab5f005263dc0d4
cedrickdeguzman <deguzmancedrick95@gmail.com>
2019-12-13 20:15:15 +0700

    refacctor(todoModel): add image

--- src/app/model/todo.model.ts
+++ src/app/model/todo.model.ts
@@ -1,9 +1,11 @@
 export class ToDo {
     title: string;
     note: string;
+    image: string;
 
     constructor(toDo) {
         this.title = toDo.title;
         this.note = toDo.note;
+        this.image = toDo.image;
     }
 }
